f6a1ac39bba5e2401e03e4df594626cf5eac9bfe
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-29 10:55:19 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //0'dan 1000000 a kadar sayıları dosyaya yaz
+            for (int i = 0; i < 731000; i++)
+            {
+                File.AppendAllText("d:\\sayilar.txt", i.ToString());
+            }
+        }
+    }
+}
